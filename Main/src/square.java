1dd90f6558e73c1ec8394bbfc274b77b503f9121
Ella Lou <sjv529@york.ac.uk>
2026-03-19 15:13:26 +0000

    Update square.java

--- Main/src/square.java
+++ Main/src/square.java
@@ -1,41 +1,21 @@
-/**
- * square class - represents a square shape
- * All sides are equal
- */
 public class square extends shape {
-    private double side;  // length of one side
-    
-    /**
-     * Create a new square
-     * @param pos the top-left corner position
-     * @param s length of one side
-     */
+    private double side;
+
     public square(coordinates pos, double s) {
-        super(4, pos);  // squares have 4 sides
+        super(4, pos); 
         this.side = s;
     }
-    
-    /**
-     * Calculate area: side × side
-     */
+
     @Override
     public double getArea() {
         return side * side;
     }
-    
-    /**
-     * Calculate perimeter: 4 × side
-     */
+ 
     @Override
     public double getPerimeter() {
         return 4 * side;
     }
-    
-    /**
-     * Make the square bigger or smaller
-     * @param factor multiply or divide by this
-     * @param sign true = multiply, false = divide
-     */
+ 
     @Override
     public void scale(int factor, boolean sign) {
         if (sign == true) {
@@ -45,13 +25,9 @@ public class square extends shape {
                 this.side = this.side / factor;
             }
         }
-        // Also scale the position
         getCoordinates().scale(factor, sign);
     }
-    
-    /**
-     * Display square information
-     */
+
     @Override
     public String display() {
         return "SQUARE: Top-Left " + getCoordinates().display() + 
@@ -59,10 +35,7 @@ public class square extends shape {
                ", Area = " + getArea() + 
                ", Perimeter = " + getPerimeter();
     }
-    
-    /**
-     * Get the side length
-     */
+
     public double getSide() {
         return side;
     }
