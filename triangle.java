df5cb794629cf636b27cc817566948ee3557c3f2
Ella Lou <sjv529@york.ac.uk>
2026-03-30 14:28:21 +0100

    Update triangle.java

--- triangle.java
+++ triangle.java
@@ -1,16 +1,17 @@
 public class triangle extends shape {
-    // A triangle has three corners
+    // Triangle defined by three vertices
     private coordinates vertex1;
     private coordinates vertex2;
     private coordinates vertex3;
 
     public triangle(coordinates v1, coordinates v2, coordinates v3) {
-        super(3, v1);  
+        super(3, v1);  // Triangle has 3 sides, position set to first vertex
         this.vertex1 = v1;
         this.vertex2 = v2;
         this.vertex3 = v3;
     }
 
+    // Private helper methods to calculate each side length
     private double sideA() {
         return vertex1.distance(vertex2);
     }
@@ -22,22 +23,24 @@ public class triangle extends shape {
     private double sideC() {
         return vertex3.distance(vertex1);
     }
- 
+
     @Override
     public double getPerimeter() {
-        return sideA() + sideB() + sideC();
+        return sideA() + sideB() + sideC();  // Sum of all three sides
     }
 
     @Override
     public double getArea() {
+        // Heron's formula: Area = √[s(s-a)(s-b)(s-c)] where s = semi-perimeter
         double a = sideA();
         double b = sideB();
         double c = sideC();
         double s = getPerimeter() / 2;
-        
+
         return Math.sqrt(s * (s - a) * (s - b) * (s - c));
     }
 
+    // Override to move all three vertices, not just the reference position
     @Override
     public void translate(int dx, int dy) {
         vertex1.translate(dx, dy);
@@ -45,9 +48,9 @@ public class triangle extends shape {
         vertex3.translate(dx, dy);
     }
 
+    // Scales all three vertex coordinates
     @Override
     public void scale(int factor, boolean sign) {
-        // Scale all three vertices
         vertex1.scale(factor, sign);
         vertex2.scale(factor, sign);
         vertex3.scale(factor, sign);
@@ -55,10 +58,10 @@ public class triangle extends shape {
 
     @Override
     public String display() {
-        return "TRIANGLE: Vertices: [" + vertex1.display() + "], [" + 
-               vertex2.display() + "], [" + vertex3.display() + "]" +
-               ", Area = " + getArea() + 
-               ", Perimeter = " + getPerimeter();
+        return "TRIANGLE: Vertices: [" + vertex1.display() + "], [" +
+                vertex2.display() + "], [" + vertex3.display() + "]" +
+                ", Area = " + getArea() +
+                ", Perimeter = " + getPerimeter();
     }
 
     public coordinates getVertex1() { return vertex1; }
